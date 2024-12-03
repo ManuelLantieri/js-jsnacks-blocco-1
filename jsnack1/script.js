63fe6e23cdb8dbe36141a0adcff1dd56b9ee531c
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-03 11:42:24 +0100

    feat: numeri uguali

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -6,6 +6,8 @@ let num2 = prompt("inserisci un'altro numero")
 
 if (num1 > num2) {
     console.log(`il numero maggiore è: ${num1}`);
-}   else {
-    console.log(`il numero maggiore è: ${num2}`)
+}   else if (num2 > num1) {
+    console.log(`il numero maggiore è: ${num2}`);
+} else {
+    console.log(`i numeri inseriti sono uguali.`)
 }
