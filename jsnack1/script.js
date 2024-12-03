3529e12728d2404f08645544bc5cac23b1464bff
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-03 12:10:53 +0100

    fix:while-do

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,26 @@
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore
 
-let num1 = prompt('inserisci un numero:')
-let num2 = prompt("inserisci un'altro numero")
+let num1;
+do {
+    num1 = parseFloat(prompt('inserisci un numero:'));
+} while(parseFloat(num1));
+
+console.log(num1)
+
+// let num1 = Number(prompt('inserisci un numero:'));
+// while(isNaN(num1)) {
+//     num1 = Number(prompt('inserisci un numero:'));
+// }
+
+
+let num2;
+
+do{
+    num2 = parseFloat(prompt("inserisci un'altro numero")) 
+} while (parseFloat(num2))
+
+
 
 if (num1 > num2) {
     console.log(`il numero maggiore è: ${num1}`);
